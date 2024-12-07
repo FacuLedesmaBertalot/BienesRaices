7704c6e495e532c9ff8e2cacce79736e540deaa5
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2024-12-07 15:18:23 -0300

    Superglobales POST y GET en php

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -5,6 +5,17 @@ require '../../includes/config/database.php';
 $db = conectarDB();
 
 
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    echo "<pre>";
+    var_dump($_POST);
+    echo "<pre>";
+
+    $titulo = $_POST['titulo'];
+    $precio = $_POST['precio'];
+}
+
+
 require '../../includes/funciones.php';
 incluirTemplate('header');
 ?>
@@ -12,18 +23,18 @@ incluirTemplate('header');
 <main class="contenedor seccion">
     <h1>Crear</h1>
 
-    
+
     <a href="/admin" class="boton boton-verde">Volver</a>
 
-    <form class="formulario">
+    <form class="formulario" method="POST" action="/admin/propiedades/crear.php">
         <fieldset>
             <legend>Información General</legend>
 
             <label for="titulo">Título:</label>
-            <input type="text" id="titulo" placeholder="Titulo Propiedad">
+            <input type="text" id="titulo" name="titulo" placeholder="Titulo Propiedad">
 
             <label for="precio">Precio:</label>
-            <input type="number" id="precio" placeholder="Precio Propiedad">
+            <input type="number" id="precio" name="precio" placeholder="Precio Propiedad">
 
             <label for="imagen">Imagen:</label>
             <input type="file" id="imagen" accept="image/jpeg, image/png">
@@ -35,13 +46,13 @@ incluirTemplate('header');
 
         <fieldset>
             <legend>Información Propiedad</legend>
-            
+
             <label for="habitaciones">Habitaciones:</label>
             <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
-            
+
             <label for="wc">Baños:</label>
             <input type="number" id="wc" placeholder="Ej: 3" min="1" max="9">
-            
+
             <label for="estacionamiento">Estacionamiento:</label>
             <input type="number" id="estacionamiento" placeholder="Ej: 3" min="1" max="9">
 
