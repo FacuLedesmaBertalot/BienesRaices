40b49acc3038fe398791d8bbb9fc69fb8c15942b
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2024-12-11 14:11:32 -0300

    Creando tabla para mostrar propiedades

--- admin/index.php
+++ admin/index.php
@@ -14,6 +14,30 @@ incluirTemplate('header');
         <?php endif; ?>
 
     <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Título</th>
+                <th>Imágen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Casa en la Playa</td>
+                <td> <img src="/imagenes/01a2fbfac8c57cad326fd62087182a8a.jpg" class="imagen-tabla"></td>
+                <td>$1200</td>
+                <td>
+                    <a href="#" class="boton-rojo-block">Eliminar</a>
+                    <a href="#" class="boton-amarillo-block">Actualizar</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </main>
 
 <?php
