bfa9c764e150684ea10ea7ba61573c9bb2c3f9ae
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2024-12-20 13:08:35 -0300

    hashear passwords

--- usuario.php
+++ usuario.php
@@ -6,14 +6,15 @@ $db = conectarDB();
 
 // Crear un email y password
 $email = "correo@correo.com";
-
 $password = "123456";
+
+$passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
 
 // Query para crear el usuario
-$query = " INSERT INTO usuarios(email, password) VALUES ('{$email}', '{$password}'); ";
+$query = " INSERT INTO usuarios(email, password) VALUES ('{$email}', '{$passwordHash}'); ";
 
-echo $query;
+// echo $query;
 
 // Agregarlo a la DB
 mysqli_query($db, $query);
