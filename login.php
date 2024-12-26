efb4f39cbeef9ad0a37e2b11abfd3d03cbd061d4
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2024-12-26 14:41:28 -0300

    Creando cerrar-sesion.php

--- login.php
+++ login.php
@@ -43,6 +43,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $_SESSION['usuario'] = $usuario['email'];
                 $_SESSION['login'] = true;
 
+                header('Location: /admin');
+
             } else {
                 $errores[]= "El password es Incorrecto";
             }
