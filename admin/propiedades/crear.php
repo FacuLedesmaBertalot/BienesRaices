1a62b14a401e3107ae0c59909158178f858ccf4f
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2025-02-20 20:53:18 -0300

    Agregando una  BD a la clase

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -119,10 +119,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
 
-
-        // Insertar en la base de datos
-        $query = " INSERT INTO propiedades (titulo, precio, imagen,  descripcion, habitaciones, wc, estacionamiento, creado, vendedores_id) VALUES ( '$titulo', '$precio', '$nombreImagen', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$creado', '$vendedores_id'  )";
-
         //echo $query;
 
         $resultado = mysqli_query($db, $query);
